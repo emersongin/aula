965d8c4833e1eb6e6936cebc4b7297eb3217d93c
= <=>
2022-03-23 21:44:53 -0300

    componentes criados

---
+++ src/screens/Main.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Form from '../components/Form';
+
+export default function Main() {
+	return (
+		<View>
+            <Form/>
+        </View>
+	);
+}
+
